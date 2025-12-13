4a8d67a2902ae26d0e1c4d2a8addf53d4d71a5b3
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-12-13 10:34:43 +0000

    Add quotes

---
+++ dom-manipulation/script.js
@@ -0,0 +1,26 @@
+const quotes = [
+  {
+    text: "Small progress is still progress.",
+    category: "Motivation",
+  },
+  {
+    text: "First, make it work. Then make it right. Then make it fast.",
+    category: "Programming",
+  },
+  {
+    text: "Winners focus on progress, not perfection.",
+    category: "Success",
+  },
+  {
+    text: "Self-control is freedom.",
+    category: "Discipline",
+  },
+  {
+    text: "Peace begins when expectations end.",
+    category: "Life",
+  },
+  {
+    text: "Knowing yourself is the beginning of all wisdom.",
+    category: "Philosophy",
+  }
+];
